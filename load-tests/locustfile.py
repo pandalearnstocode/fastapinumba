3e75784f359849c38c845b62702f0ec80acf053b
Aritra Biswas <aritra.biswas@ab-inbev.com>
2021-02-13 16:54:53 +0530

    minor changes in api layer.

--- load-tests/locustfile.py
+++ load-tests/locustfile.py
@@ -3,15 +3,15 @@ from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
-    @task
+    @task(10)
     def put_nb_vectorized(self):
         self.client.post("/nb_vectorized", json = {"n": 10000})
-    @task
-    def put_nb_vectorized(self):
+    @task(10)
+    def put_nb_jit(self):
         self.client.post("/nb_jit", json = {"n": 10000})
-    @task
-    def put_nb_vectorized(self):
+    @task(10)
+    def put_nb_njit(self):
         self.client.post("/nb_njit", json = {"n": 10000})
-    @task
-    def put_nb_vectorized(self):
+    @task(10)
+    def put_nb_njit_fm(self):
         self.client.post("/nb_njit_fm", json = {"n": 10000})
