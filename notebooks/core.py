e0df16d26590b368f136dd36765cf8cf58c6ea0b
Aritra Biswas <pandalearnstocode@gmail.com>
2021-02-13 18:13:05 +0530

    adding test related stuff.

--- notebooks/core.py
+++ notebooks/core.py
@@ -1,5 +1,17 @@
 import numba as nb
+import pandas as pd
 import numpy as np
+from loguru import logger
+import numexpr as ne
+import sympy
+import scipy
+from math import * 
+logger.info(f"Pandas version: {pd.__version__}")
+logger.info(f"Numpy version: {np.__version__}")
+logger.info(f"Numba version: {ne.__version__}")
+logger.info(f"NumExpr version: {nb.__version__}")
+logger.info(f"SymPy version: {sympy.__version__}")
+logger.info(f"SciPy version: {scipy.__version__}")
 
 def softplus_np(n):
     x = np.random.uniform(50,150,size = n).astype(np.float32)
@@ -11,7 +23,7 @@ def softplus_vec_wrap(n):
     @nb.vectorize('float32(float32)',target = "parallel")
     def softplus_nb(x):
         safe = x < 100
-        return np.log(1 + np.exp(x * safe)) * safe + x * (1-safe)
+        return log(1 + exp(x * safe)) * safe + x * (1-safe)
     result = softplus_nb(x)
     return result
 
