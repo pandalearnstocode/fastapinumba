959aa833c298150cad96e73f6960cc59e83c4b15
Aritra Biswas <aritra.biswas@ab-inbev.com>
2021-02-13 15:05:55 +0530

    adding basic api layer.

--- api/main.py
+++ api/main.py
@@ -14,24 +14,20 @@ def root():
 
 @app.post("/nb_vectorized/")
 def nb_vec_api(sim_params: SimulationParameters):
-    sum_x = core.softplus_vec_wrap(sim_params.n)
-    print(np.ma.masked_invalid(sum_x).sum())
-    return sim_params
+    result = np.ma.masked_invalid(core.softplus_vec_wrap(sim_params.n)).sum()
+    return {"sum": result}
 
 @app.post("/nb_jit/")
 def nb_jit_api(sim_params: SimulationParameters):
-    sum_x = core.softplus_vec_wrap(sim_params.n)
-    print(np.ma.masked_invalid(sum_x).sum())
-    return sim_params
+    result = np.ma.masked_invalid(core.softplus_vec_wrap(sim_params.n)).sum()
+    return {"sum": result}
 
 @app.post("/nb_njit/")
 def nb_njit_api(sim_params: SimulationParameters):
-    sum_x = core.softplus_vec_wrap(sim_params.n)
-    print(np.ma.masked_invalid(sum_x).sum())
-    return sim_params
+    result = np.ma.masked_invalid(core.softplus_vec_wrap(sim_params.n)).sum()
+    return {"sum": result}
 
 @app.post("/nb_njit_fm/")
 def nb_njit_fm_api(sim_params: SimulationParameters):
-    sum_x = core.softplus_fastmath_njit(sim_params.n)
-    print(np.ma.masked_invalid(sum_x).sum())
-    return sim_params
+    result = np.ma.masked_invalid(core.softplus_fastmath_njit(sim_params.n)).sum()
+    return {"sum": result}
