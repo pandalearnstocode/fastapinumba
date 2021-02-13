2a9e55d4691f0c71cd0bbdb9c33fc4dfa7e6a071
Aritra Biswas <aritra.biswas@ab-inbev.com>
2021-02-13 16:35:52 +0530

    adding other endpoints.

--- load-tests/locustfile.py
+++ load-tests/locustfile.py
@@ -5,4 +5,13 @@ class QuickstartUser(HttpUser):
 
     @task
     def put_nb_vectorized(self):
-        self.client.post("/nb_vectorized", json = {"n": 100000})
+        self.client.post("/nb_vectorized", json = {"n": 10000})
+    @task
+    def put_nb_vectorized(self):
+        self.client.post("/nb_jit", json = {"n": 10000})
+    @task
+    def put_nb_vectorized(self):
+        self.client.post("/nb_njit", json = {"n": 10000})
+    @task
+    def put_nb_vectorized(self):
+        self.client.post("/nb_njit_fm", json = {"n": 10000})
