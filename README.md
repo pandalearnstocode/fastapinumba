484884aa27a15ee9ec04afe6d47387d9f30947f2
Aritra Biswas <aritra.biswas@ab-inbev.com>
2021-02-13 14:14:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Resources:
+
+* Numba: https://www.youtube.com/watch?v=mDsTkg_IpUk
+* Creating snippets in VSC: https://www.youtube.com/watch?v=JIqk9UxgKEc
+* Numba Repo: https://github.com/numba/numba-examples/tree/master/notebooks
---
+++ notebooks/core.py
@@ -0,0 +1,32 @@
+import numba as nb
+import pandas as pd
+import numpy as np
+from loguru import logger
+import numexpr as ne
+import sympy
+import scipy
+from math import * 
+logger.info(f"Pandas version: {pd.__version__}")
+logger.info(f"Numpy version: {np.__version__}")
+logger.info(f"Numba version: {ne.__version__}")
+logger.info(f"NumExpr version: {nb.__version__}")
+logger.info(f"SymPy version: {sympy.__version__}")
+logger.info(f"SciPy version: {scipy.__version__}")
+
+def softplus_np(n):
+    x = np.random.uniform(50,150,size = n).astype(np.float32)
+    safe = x < 100
+    return np.log(1 + np.exp(x * safe)) * safe + x * (1-safe)
+
+def softplus_vec_wrap(n):
+    x = np.random.uniform(50,150,size = n).astype(np.float32)
+    @nb.vectorize('float32(float32)',target = "parallel")
+    def softplus_nb(x):
+        safe = x < 100
+        return log(1 + exp(x * safe)) * safe + x * (1-safe)
+    result = softplus_nb(x)
+    return result
+
+softplus_jit = nb.jit(softplus_np)
+softplus_njit = nb.njit(softplus_np)
+softplus_fastmath_njit = nb.njit(fastmath=True,parallel=True)(softplus_np)
